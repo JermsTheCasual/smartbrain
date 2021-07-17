39eef9d6379cb6dd3f9cb9735b7199db78492ee8
Jeremy Taylor <jeremy.taylor92@gmail.com>
2021-07-17 13:36:20 -0400

    api changes

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignin = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://limitless-badlands-30868.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
